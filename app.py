c7dc2876f6ab45276195ad139f3b16a2c9ef7376
tkongolo24 <t.kongolo@alustudent.com>
2026-03-18 12:14:45 +0200

    Enhance: add missing blueprints for stats and auth routes in app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 from backend.routes.property import property_bp
 from backend.routes.unit import unit_bp
 from backend.routes.tenant import tenant_bp
 from backend.routes.ticket import ticket_bp
 from backend.routes.communication import communication_bp
+from backend.routes.stats import stats_bp
+from backend.routes.auth import auth_bp
+
+from backend.routes.seed import seed_bp
 
 # load environment variables
 load_dotenv()
 
 # Initialize flask
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 api_version = '/api/v1'
@@ -24,7 +30,10 @@ app.register_blueprint(unit_bp, url_prefix=api_version)
 app.register_blueprint(tenant_bp, url_prefix=api_version)
 app.register_blueprint(ticket_bp, url_prefix=api_version)
 app.register_blueprint(communication_bp, url_prefix=api_version)
+app.register_blueprint(stats_bp, url_prefix=api_version)
+app.register_blueprint(auth_bp, url_prefix=api_version)
 
+app.register_blueprint(seed_bp, url_prefix=api_version)
 
 if __name__ == '__main__':
     app.run(debug=True)
